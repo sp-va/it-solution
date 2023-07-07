5094a1129c4f10e29683e256f719eb195e0623b0
spirin-vadim <vadis99@ya.ru>
2023-07-07 21:18:37 +0500

    upd

--- runline/run/views.py
+++ runline/run/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import InputTextForLine
 from django.views.decorators.csrf import csrf_exempt
-from .text import video_maker
+#from .text import video_maker
 
 
 
@@ -12,7 +12,7 @@ def create_video(request):
     if request.method == 'POST':
         form = InputTextForLine(request.POST)
         if form.is_valid():
-            video_maker('234')
+            #video_maker('234')
             form.save()
             return redirect('create_video')
 
--- text.py
+++ text.py
@@ -1,29 +1,24 @@
-from PIL import Image
-from moviepy.editor import *
+import cv2
+from PIL import Image, ImageFont, ImageDraw
 
+def video_maker(text):
+    frame = (100, 100)
+    output = cv2.VideoWriter('output_video.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, frame)
+    width = 0
+    font = ImageFont.truetype('arial.ttf', 40)
+    for i in text:
+        width += font.getlength(i)
+    x = 100
+    for i in range(0, 90):
+        image = Image.new('RGB', (100, 100), (100, 50, 13))
+        draw = ImageDraw.Draw(image)
 
-def video_maker(input_txt):
-    pic = Image.new('RGB', (100, 100), 'green')
-    pic.save('my_image.png')
-    image = ['my_image.png', ]
-    clip = [ImageClip(m).set_duration(3) for m in image]
-    concatenated = concatenate_videoclips(clip, method='compose')
-    concatenated.write_videofile('my_video.mp4', fps=30)
+        draw.text((x, 50), text, font=font)
+        image.save('my_image.png')
+        image = cv2.imread("my_image.png")
+        output.write(image)
+        x -= (100+width)/90
+    print(width)
+    output.release()
 
-    txt = input_txt
-
-    w = 100
-    h = 50
-
-    clip = VideoFileClip("my_video.mp4")
-
-    txt_clip = TextClip(
-        txt, color="white", fontsize=25, font="arial.ttf"
-    ).set_duration(3)
-
-    txt_clip = txt_clip.set_pos(lambda t: (int(-w * t + 100),
-                                           int(5.4 * h / 6)))
-
-    video = CompositeVideoClip([clip, txt_clip])
-
-    video.write_videofile('my_video.mp4')
+video_maker('eijgoerjgioreheigherogjreign')
