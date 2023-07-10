c883f8ced131fb7dc9579be3d06f3c9503173fb3
spirin-vadim <138710260+spirin-vadim@users.noreply.github.com>
2023-07-10 19:05:15 +0500

    Update settings.py

--- runline/settings.py
+++ runline/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)x25&kx(cy3eaxy)3f&z^l#v941n13h8f#volzt-56#vuur!+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
